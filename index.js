75209b605de5c6fff7ba76056069eafe514472ea
Alex Peterson <alexcpeterson7@gmail.com>
2020-11-05 21:25:35 -0500

    add mongoose

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 var birds = require('./routes/test-route')
+const mongoose = require('mongoose');
+require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 3000;
+
+const uri = process.env.ATLAS_URI
+
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
