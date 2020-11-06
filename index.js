a6c7d3c29adc99b3f5f3749dc1761645018c673e
Alex Peterson <alexcpeterson7@gmail.com>
2020-11-05 20:09:36 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+var birds = require('./routes/test-route')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.use('/birds', birds)
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
